81c45a072f33e447a4e4ed9a2ec2949c5fb7de9d
Mafouz DIALLO <mafouzdiallo@gmail.com>
2022-05-03 15:42:44 +0000

    mise en place du frontend avec react

---
+++ frontend/src/pages/Page404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page404 = () => {
+  return (
+    <div className='content'>Page 404</div>
+  )
+}
+
+export default Page404
---
+++ frontend/src/pages/View.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const View = () => {
+  return (
+    <div className='content'>View</div>
+  )
+}
+
+export default View
